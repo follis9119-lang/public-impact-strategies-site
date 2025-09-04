5d88ff550fbaa2efbc718a813900a09ea49e07fd
follis9119-lang <follis9119@gmail.com>
2025-09-04 16:57:29 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'system-ui' }}>
+      <h1>Public Impact Strategies</h1>
+      <p>Empowering public-minded organizations to make measurable impact.</p>
+    </main>
+  );
+}
